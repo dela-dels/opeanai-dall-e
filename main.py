0174f390f1f27f816875b27b4e3f78f5a90a8df3
Dela Akakpo <delprane14@gmail.com>
2023-07-19 07:54:17 +0000

    retrieve urls from response

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# openai initial set up with organisation id and api keys
 openai.organization = os.environ['OPENAI_ORGANISATION_ID']
 openai.api_key = os.environ['OPENAI_API_KEY']
 
 response = openai.Image.create(
-    prompt="An abstract image for a birthday card",
+    prompt="A beautiful image of a painting to be used on a birthday card",
     n=2,
     size=os.environ['OPENAI_IMAGE_SIZE_1024_1024']
 )
 
-print(response)
+# response = openai.Completion.create(
+#     model="text-davinci-003", prompt="write a beautiful birthday message to be printed on a birthday card")
+
+print(response.data[0].url)
